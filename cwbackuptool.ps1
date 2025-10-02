b5a76db862c87816aa07f7dff2faa3f0bd55a6a4
Luna Rose <luna.rose@lunros.me>
2025-10-02 10:25:24 -0700

    Update cwbackuptool.ps1
    Add license.

--- cwbackuptool.ps1
+++ cwbackuptool.ps1
@@ -3,6 +3,15 @@
 # This script provides a menu to:
 # 1. Backup: Finds the specific IndexedDB folder, stops Overwolf, and compresses the folder into a .zip file on your Desktop.
 # 2. Restore: Stops Overwolf, asks for a backup .zip file, deletes the current IndexedDB folder, and extracts the backup in its place.
+#MIT License
+#
+# Copyright (c) 2025 Luna Rose
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 # --- Configuration ---
 # Construct the path to the IndexedDB folder using the %localappdata% environment variable.
@@ -134,5 +143,6 @@ while ($true) {
     $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
     Clear-Host
 }
+
 
 
